33d4b46d43cf813bebfc97b30f46f4f56fde6260
Phoenix <Phoenix.com>
2022-09-30 17:46:26 +0800

    master commit2

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git");
         System.out.println("fot-fix分支");
-        System.out.println("分支冲突——fot");
+        System.out.println("分支冲突——master");
     }
 }
